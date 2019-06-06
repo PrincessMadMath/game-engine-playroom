f781366a2fb6575e1cc101bf66fe8f8598d3e0f1
Mathieu Gamache <mathieu.gamache@gsoft.com>
2019-06-05 20:26:00 -0400

    Trying to fit phaser in React

--- browser-client/app/src/DrinkOrPiss/GameViewer.jsx
+++ browser-client/app/src/DrinkOrPiss/GameViewer.jsx
@@ -1,29 +1,38 @@
 import Phaser from "phaser";
-import React from "react";
+import React, { useEffect } from "react";
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-const game = new Phaser.Game(config);
-function preload() {
+export function GameViewer({ gameState }) {
+    useEffect(() => {
+        const config = {
+            type: Phaser.AUTO,
+            parent: "phaser-game",
+            width: 800,
+            height: 600,
+            scene: {
+                preload: preload,
+                create: create,
+                update: update
+            }
+        };
 
+        new Phaser.Game(config);
+    }, []);
+
+    return (
+        <div>
+            <div id="phaser-game" />
+        </div>
+    );
 }
 
+function preload() {}
+
+function update() {}
+
 function create() {
-    var drinkCountText =  this.add.text(0,this.scale.height - 200, "Drink", {
+    var drinkCountText = this.add.text(0, this.scale.height - 200, "Drink", {
         font: "32px Arial",
         fill: "#ff0044",
         align: "center"
     });
-
-}   
-export function GameViewer({ gameState }) {
-    return <div>{<span>{JSON.stringify(gameState)}</span>}</div>;
 }
