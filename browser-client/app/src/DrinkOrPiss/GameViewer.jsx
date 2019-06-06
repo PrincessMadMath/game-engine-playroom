53dc06a0f5ed1306c1cc4a89e49b1ddc40d5f575
Alexandre Sanscartier <alex.sanscartier@gsoft.com>
2019-06-05 20:15:10 -0400

    Added Phaser to the react

--- browser-client/app/src/DrinkOrPiss/GameViewer.jsx
+++ browser-client/app/src/DrinkOrPiss/GameViewer.jsx
@@ -1,5 +1,29 @@
-import React, { useState } from "react";
+import Phaser from "phaser";
+import React from "react";
 
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+const game = new Phaser.Game(config);
+function preload() {
+
+}
+
+function create() {
+    var drinkCountText =  this.add.text(0,this.scale.height - 200, "Drink", {
+        font: "32px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+
+}   
 export function GameViewer({ gameState }) {
     return <div>{<span>{JSON.stringify(gameState)}</span>}</div>;
 }
